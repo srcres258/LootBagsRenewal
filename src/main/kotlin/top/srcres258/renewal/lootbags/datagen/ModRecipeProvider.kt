fd18cd4ad276855d94cab014a7d20363ea887016
src_resources <src.res.211@gmail.com>
2025-03-23 22:00:14 +0800

    Add recipes for blocks

--- src/main/kotlin/top/srcres258/renewal/lootbags/datagen/ModRecipeProvider.kt
+++ src/main/kotlin/top/srcres258/renewal/lootbags/datagen/ModRecipeProvider.kt
@@ -2,12 +2,47 @@ package top.srcres258.renewal.lootbags.datagen
 
 import net.minecraft.core.HolderLookup
 import net.minecraft.data.PackOutput
+import net.minecraft.data.recipes.RecipeCategory
+import net.minecraft.data.recipes.RecipeOutput
 import net.minecraft.data.recipes.RecipeProvider
+import net.minecraft.data.recipes.ShapedRecipeBuilder
+import net.minecraft.world.item.Items
+import net.minecraft.world.level.block.Blocks
 import net.neoforged.neoforge.common.conditions.IConditionBuilder
+import top.srcres258.renewal.lootbags.block.ModBlocks
 import java.util.concurrent.CompletableFuture
 
 class ModRecipeProvider(
     output: PackOutput,
     registries: CompletableFuture<HolderLookup.Provider>
 ) : RecipeProvider(output, registries), IConditionBuilder {
+    override fun buildRecipes(recipeOutput: RecipeOutput) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.LOOT_RECYCLER)
+            .pattern("AAA")
+            .pattern("ABA")
+            .pattern("ACA")
+            .define('A', Blocks.STONE)
+            .define('B', Items.CHEST)
+            .define('C', Items.IRON_INGOT)
+            .unlockedBy("has_chest", has(Items.CHEST))
+            .save(recipeOutput)
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.BAG_OPENER)
+            .pattern("ACA")
+            .pattern("ABA")
+            .pattern("AAA")
+            .define('A', Blocks.STONE)
+            .define('B', Items.CHEST)
+            .define('C', Items.IRON_INGOT)
+            .unlockedBy("has_chest", has(Items.CHEST))
+            .save(recipeOutput)
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.BAG_STORAGE)
+            .pattern("AAA")
+            .pattern("CBC")
+            .pattern("AAA")
+            .define('A', Blocks.STONE)
+            .define('B', Items.CHEST)
+            .define('C', Items.IRON_INGOT)
+            .unlockedBy("has_chest", has(Items.CHEST))
+            .save(recipeOutput)
+    }
 }
